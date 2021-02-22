e4533529d3607428d8158c1facb601cf842fbb07
unknown <victormalych@gmail.com>
2021-02-22 12:30:20 +0000

    Adjusted heartbeat detection threshold and added an IPI sequence array

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -1,19 +1,21 @@
 #define USE_ARDUINO_INTERRUPTS true
+#define LENGTH 6
+#define GROUP 2
 #include <PulseSensorPlayground.h>
 
 //  Variables
 int Signal;                     // holds the incoming raw data. Signal value can range from 0-1024
 const int PULSE_INPUT = A0;
 const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
-const int THRESHOLD = 515;   // Determine which Signal to "count as a beat", and which to ingore.
+const int THRESHOLD = 530;   // Determine which Signal to "count as a beat", and which to ingore.
 int LED13 = 13;                 //  The on-board Arduino LED
 const int OUTPUT_TYPE = SERIAL_PLOTTER;
 PulseSensorPlayground pulseSensor;
 
-
-boolean peakDetected = false;
 int bpm = 0;
 int ibi = 0;
+int sequence[LENGTH];
+int step = 0;
 
 void setup() 
 {
@@ -44,18 +46,32 @@ void setup()
 void loop() 
 {
   delay(20);
-  //pulseSensor.outputSample();
+  pulseSensor.outputSample();
   if (pulseSensor.sawStartOfBeat()) 
   {
-    //pulseSensor.outputBeat();
+    pulseSensor.outputBeat();
     bpm = pulseSensor.getBeatsPerMinute();
     ibi = pulseSensor.getInterBeatIntervalMs();
-    Serial.println(ibi);
+    
+    if (step != LENGTH)
+    {
+      sequence[step] = ibi;
+      step++;
+    }
+    else
+    {
+      step = 0;
+      encrypt();
+    }
   }
 }
 
-// Peak detection function. Reads the signal coming from the sensor and returns true if there's a heartbeat.
-boolean detectPeak()
+void encrypt()
 {
-  return false;
+  for (int i = 0; i < LENGTH; i++)
+  {
+    Serial.print(sequence[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
 }
