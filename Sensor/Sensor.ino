a3f4838a4c259160f1ca335f1bd2805abd87ec4e
unknown <victormalych@gmail.com>
2021-02-23 12:09:16 +0000

    IPI sequence grouping

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -7,7 +7,7 @@
 int Signal;                     // holds the incoming raw data. Signal value can range from 0-1024
 const int PULSE_INPUT = A0;
 const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
-const int THRESHOLD = 530;   // Determine which Signal to "count as a beat", and which to ingore.
+const int THRESHOLD = 515;   // Determine which Signal to "count as a beat", and which to ingore.
 int LED13 = 13;                 //  The on-board Arduino LED
 const int OUTPUT_TYPE = SERIAL_PLOTTER;
 PulseSensorPlayground pulseSensor;
@@ -68,10 +68,24 @@ void loop()
 
 void encrypt()
 {
+  Serial.println("IPI Sequence");
   for (int i = 0; i < LENGTH; i++)
   {
     Serial.print(sequence[i]);
     Serial.print(" ");
+  }
+  Serial.println();
+
+  //Group the IPI sequence
+  int groupedSequence[LENGTH/GROUP];
+  int j = 0;
+  Serial.println("Grouped");
+  for (int i = 0; i < LENGTH; i += 2)
+  {
+    groupedSequence[j] = sequence[i] + sequence[i + 1];
+    Serial.print(groupedSequence[j]);
+    Serial.print(" ");
+    j++;
   }
   Serial.println();
 }
