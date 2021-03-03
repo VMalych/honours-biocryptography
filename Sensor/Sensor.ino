b01997892f0fe2ed891b1f103cdcc20d8f3eb2f9
unknown <victormalych@gmail.com>
2021-03-03 10:56:39 +0000

    Added second PulseSensor to IoT device

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -8,12 +8,13 @@
 #include <PulseSensorPlayground.h>
 //  Variables
 int Signal;                     // holds the incoming raw data. Signal value can range from 0-1024
-const int PULSE_INPUT = A0;
+const int PULSE_INPUT1 = A0;
+const int PULSE_INPUT2 = A1;
 const int PULSE_BLINK = 13;    // Pin 13 is the on-board LED
 const int THRESHOLD = 515;   // Determine which Signal to "count as a beat", and which to ingore.
 int LED13 = 13;                 //  The on-board Arduino LED
 const int OUTPUT_TYPE = SERIAL_PLOTTER;
-PulseSensorPlayground pulseSensor;
+PulseSensorPlayground pulseSensor(2);
 
 int bpm = 0;
 int ibi = 0;
@@ -25,8 +26,9 @@ void setup()
   Serial.begin(115200);
 
   // Configure the PulseSensor manager.
-  pulseSensor.analogInput(PULSE_INPUT);
-  pulseSensor.blinkOnPulse(PULSE_BLINK);
+  pulseSensor.analogInput(PULSE_INPUT1, 0);
+  pulseSensor.blinkOnPulse(PULSE_BLINK, 0);
+  pulseSensor.analogInput(PULSE_INPUT2, 1);
   pulseSensor.setSerial(Serial);
   pulseSensor.setOutputType(OUTPUT_TYPE);
   pulseSensor.setThreshold(THRESHOLD);
